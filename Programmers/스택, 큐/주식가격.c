6924c69198278601eff450be3dbd4771c333827a
hangenie <jehan2672@gmail.com>
2021-04-29 10:48:16 +0900

    [Programmers] 주식가격

---
+++ Programmers/스택, 큐/주식가격.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// prices_len은 배열 prices의 길이입니다.
+int* solution(int prices[], size_t prices_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * prices_len);
+    int cnt = 0;
+
+    for (int i = 0; i < prices_len; i++) {
+        for (int j = i + 1; j < prices_len; j++) {
+            if (prices[i] <= prices[j]) {
+                cnt++;
+            }
+            else {
+                cnt++;
+                break;
+            }
+        }
+        answer[i] = cnt;
+        cnt = 0;
+    }
+
+    return answer;
+}
