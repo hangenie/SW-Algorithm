16df996a59f4cdf73c8ef2e3eee7d72e4815e271
hangenie <jehan2672@gmail.com>
2021-04-29 10:44:06 +0900

    [Programmers] 위장

---
+++ Programmers/해시/위장.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+
+    map<string, int> m;
+
+    for (auto c : clothes) {
+        m[c[1]]++;
+    }
+
+    for (auto it = m.begin(); it != m.end(); it++) {
+        answer *= it->second + 1;
+    }
+
+    answer -= 1;
+
+    return answer;
+}
